8cf65ab60d3780145569fc01e8093b6ba7c4499d
Alex <oleksandr.kravtsov@ovogenebank.com>
2026-05-12 10:46:16 +0300

    зміна назви

--- calculator.py
+++ calculator.py
@@ -1,2 +1,30 @@
-# Мій перший калькулятор на Python
- 
+def calculator():
+    print("--- Простий калькулятор на Python ---")
+    
+    try:
+        num1 = float(input("Введіть перше число: "))
+        operator = input("Введіть дію (+, -, *, /): ")
+        num2 = float(input("Введіть друге число: "))
+
+        if operator == '+':
+            result = num1 + num2
+        elif operator == '-':
+            result = num1 - num2
+        elif operator == '*':
+            result = num1 * num2
+        elif operator == '/':
+            # Обробка ділення на нуль
+            if num2 == 0:
+                return "Помилка: на нуль ділити не можна!"
+            result = num1 / num2
+        else:
+            return "Помилка: невірна операція."
+
+        return f"Результат: {result}"
+
+    except ValueError:
+        return "Помилка: вводьте тільки числа."
+
+# Запуск програми
+print(calculator())
+
